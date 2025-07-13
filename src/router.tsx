762bb6b35cc72f557a4c902b821f5f01a40733b1
Guilherme <guilherme.s.barros@outlook.com>
2025-07-12 22:20:29 -0300

    feat: reset scroll

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,37 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { type ReactNode, useLayoutEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 
 import { DefaultLayout } from './layout/default'
 import { Checkout } from './pages/checkout'
 import { Home } from './pages/home'
 import { Success } from './pages/success'
 
+interface WrapperProps {
+	children: ReactNode
+}
+
+const Wrapper = ({ children }: WrapperProps) => {
+	const location = useLocation()
+
+	useLayoutEffect(() => {
+		window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+	}, [location.pathname])
+
+	return children
+}
+
 export function Router() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" Component={DefaultLayout}>
-					<Route index path="/" Component={Home} />
-					<Route path="/checkout" Component={Checkout} />
-					<Route path="/checkout/success" Component={Success} />
-				</Route>
-			</Routes>
+			<Wrapper>
+				<Routes>
+					<Route path="/" Component={DefaultLayout}>
+						<Route index path="/" Component={Home} />
+						<Route path="/checkout" Component={Checkout} />
+						<Route path="/checkout/success" Component={Success} />
+					</Route>
+				</Routes>
+			</Wrapper>
 		</BrowserRouter>
 	)
 }
